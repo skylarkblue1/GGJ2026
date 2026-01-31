965399cd396ac79b3c9fa571117386159793d49a
Sky <15182062+skylarkblue1@users.noreply.github.com>
2026-01-31 14:47:24 +0000

    Item Placement

--- Assets/Scripts/Player/InteractionController.cs
+++ Assets/Scripts/Player/InteractionController.cs
@@ -7,6 +7,8 @@ public class InteractionController : MonoBehaviour
 
     private GameObject interactable;
 
+    public Sprite ladder;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "interactable")
@@ -27,14 +29,28 @@ public class InteractionController : MonoBehaviour
 
     void OnInteract()
     {
+        Tags tags = interactable.GetComponent<Tags>();
+        Sprite sprite = interactable.GetComponent<SpriteRenderer>().sprite;
+
         Debug.Log("Interacting");
-        if (inRange && inventory.heldObject == "Empty")
+        if (inRange && inventory.heldObject == "Empty" && tags.tags[1] == "pickup")
         {
             Debug.Log("trying to interact");
-            Tags tags = interactable.GetComponent<Tags>();
             inventory.heldObject = tags.tags[0];
             Debug.Log("New held object is: " + inventory.heldObject);
             interactable.SetActive(false);
+        }
+
+        // Any holy entities please forgive me for the coding sins I'm about to commit in this spaghetti code below
+
+        if (inRange && inventory.heldObject == tags.tags[0] && tags.tags[1] == "placement")
+        {
+            if (inventory.heldObject == "ladder")
+            {
+                Debug.Log("Placing ladder");
+                inventory.heldObject = "Empty";
+                sprite = ladder; //not... changing sprite? Hopefully it works with actual sprites or idk
+            } // else if (all the other items listed)
         }
     }
 }

021c94bbda56481b5498a8468784fd3afaa27c74
Sky <15182062+skylarkblue1@users.noreply.github.com>
2026-02-01 02:08:37 +0000

    Assets in, Living room laid out ready for detection scripts

--- Assets/Scripts/Player/InteractionController.cs
+++ Assets/Scripts/Player/InteractionController.cs
@@ -14,12 +14,17 @@ public class InteractionController : MonoBehaviour
     public Sprite key;
     public Sprite photo;
 
+    public Sprite windowOpen;
+    public Sprite windowClose;
+
     private SpriteRenderer self;
 
     public bool hiding = false;
 
     private Tags tags;
     private Sprite sprite;
+
+    public SpriteRenderer ePrompt;
 
     public void Awake()
     {
@@ -32,6 +37,8 @@ public class InteractionController : MonoBehaviour
         {
             inRange = true;
             interactable = other.gameObject;
+
+            ePrompt.enabled = true;
 
             tags = interactable.GetComponent<Tags>();
             sprite = interactable.GetComponent<SpriteRenderer>().sprite;
@@ -60,6 +67,8 @@ public class InteractionController : MonoBehaviour
         {
             inRange = false;
             interactable = null;
+
+            ePrompt.enabled = false;
         }
     }
 
@@ -72,6 +81,15 @@ public class InteractionController : MonoBehaviour
             inventory.heldObject = tags.tags[0];
             Debug.Log("New held object is: " + inventory.heldObject);
             interactable.SetActive(false);
+        }
+
+        if (inRange && tags.tags[0] == "window")
+        {
+            if (tags.tags[1] == "toopen")
+            {
+                interactable.GetComponent<SpriteRenderer>().sprite = windowOpen;
+                //Start playing window sound
+            }
         }
 
         // Any holy entities please forgive me for the coding sins I'm about to commit in this spaghetti code below
